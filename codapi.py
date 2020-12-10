45c6ad8010925b237614739aa3f53b8127cd2873
Jack Wiig <jhwiig@gmail.com>
2020-12-10 17:49:05 -0500

    implemented /api/label-track

--- codapi.py
+++ codapi.py
@@ -1,7 +1,11 @@
 from flask import Flask, request
+import numpy as np
+
+from typing import List
+from predict import IALModel, MODEL_PATH, CLASSLIST_PATH
 
 app = Flask(__name__)
-
+model = IALModel(MODEL_PATH, CLASSLIST_PATH)
 
 @app.route('/')
 def hello_world():
@@ -9,4 +13,17 @@ def hello_world():
 
 @app.route('/api/label-track', methods=['POST'])
 def label_track():
-    pass
+    audio: List = list(request.form['buffer'])
+    sr: float = request.form['sampleRate']
+
+    audio_array = np.asarray(audio)
+    labels: List[str] = model.predict_from_audio_array(audio_array, sr)
+
+    ret = {'labels':[]}
+
+    last_label = ''
+    for time, label in enumerate(labels):
+        if label != last_label:
+            ret['labels'].append({'label':label, 'start':time})
+
+    return ret
